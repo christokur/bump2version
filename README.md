1c4d7e762ec46c773b1c30abc3bf38baf44feed2
Christo De Lange <christo.delange@sands.com>
2024-09-04 12:58:04 -0400

    chore: Bump version: 1.0.2-beta5 → 1.0.2-beta6

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# bump2version 1.0.2-beta5
+# bump2version 1.0.2-beta6
 
 ## NOTE
 
--- bumpversion/__init__.py
+++ bumpversion/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "1.0.2-beta5"
+__version__ = "1.0.2-beta6"
 __license__ = "MIT"
 __title__ = "bumpversion"
